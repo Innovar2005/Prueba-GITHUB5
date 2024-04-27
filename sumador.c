660a82ad2edcb89c8970a52155653b2696960588
Gustavo Fossati <gustavo.fossati@alu.inspt.utn.edu.ar>
2024-04-27 20:46:26 -0300

    Actualizacion del sumador para tres sumandos

--- sumador.c
+++ sumador.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int x1, x2, suma;
+int x1, x2, x3, suma;
 int main()
 {
     printf("Ingresar un primer valor \n");
@@ -9,7 +9,10 @@ int main()
     printf("Ingresar un segundo valor \n");
     scanf("%d", &x2);
 
-    suma = x1 + x2;
+    printf("Ingresar un primer valor \n");
+    scanf("%d", &x3);
+
+    suma = x1 + x2 + x3;
 
     printf("La suma resulta = %d ", suma);
 
